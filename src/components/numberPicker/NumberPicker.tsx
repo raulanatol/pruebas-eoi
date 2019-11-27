6cafb57f9536c7b431effda295953dab9c236003
raulanatol <raul@natol.es>
2019-11-27 20:05:16 +0000

    New feature done! 🍺

--- src/components/numberPicker/NumberPicker.tsx
+++ src/components/numberPicker/NumberPicker.tsx
@@ -1,29 +1,35 @@
 import React from "react";
 
+interface NumberPickerProps {
+  initialCounter: number;
+}
+
 interface NumberPickerState {
   counter: number;
 }
 
-export class NumberPicker extends React.Component<{}, NumberPickerState> {
+export class NumberPicker extends React.Component<NumberPickerProps, NumberPickerState> {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      counter: this.props.initialCounter
     };
     this.incrementCounter = this.incrementCounter.bind(this);
     this.decrementCounter = this.decrementCounter.bind(this);
   }
 
-  incrementCounter() {
+  updateCounter(delta: number) {
     this.setState({
-      counter: this.state.counter + 1
+      counter: this.state.counter + delta
     });
   }
 
   decrementCounter() {
-    this.setState({
-      counter: this.state.counter - 1
-    });
+    this.updateCounter(-1);
+  }
+
+  incrementCounter() {
+    this.updateCounter(1);
   }
 
   render() {
--- src/components/numberPicker/__tests__/NumberPicker.test.tsx
+++ src/components/numberPicker/__tests__/NumberPicker.test.tsx
@@ -5,30 +5,39 @@ import { NumberPicker } from "../NumberPicker";
 describe("NumberPicker", () => {
   describe("render", () => {
     test("Deberia renderizar un Button", () => {
-      const wrapper = shallow(<NumberPicker/>);
+      const wrapper = shallow(<NumberPicker initialCounter={0}/>);
       expect(wrapper.exists('button')).toBeTruthy();
     });
     test("Deberia renderizar un Button con un texto de un +", () => {
-      const wrapper = shallow(<NumberPicker/>);
+      const wrapper = shallow(<NumberPicker initialCounter={0}/>);
       expect(wrapper.find('button').at(0).text()).toBe('+');
     });
     test("Deberia renderizar otro button con un texto de un -", () => {
-      const wrapper = shallow(<NumberPicker/>);
+      const wrapper = shallow(<NumberPicker initialCounter={0}/>);
       expect(wrapper.find('button').at(1).text()).toBe('-');
     });
     test("Deberia renderizar un h1 con el texto 0", () => {
-      const wrapper = shallow(<NumberPicker/>);
+      const wrapper = shallow(<NumberPicker initialCounter={0}/>);
       expect(wrapper.find('h1').text()).toBe('0');
     });
-    test("Deberia renderizar un 1 cuando se haya hecho un click en el boton +", () => {
-      const wrapper = shallow(<NumberPicker/>);
-      wrapper.find("button").at(0).simulate("click");
-      expect(wrapper.find("h1").text()).toBe("1");
-    });
-    test("Deberia renderizar un -1 cuando se haya hecho un click en el boton -", () => {
-      const wrapper = shallow(<NumberPicker/>);
-      wrapper.find("button").at(1).simulate("click");
-      expect(wrapper.find("h1").text()).toBe("-1");
+    describe('Increment/decrement', () => {
+      test("Deberia renderizar un 1 cuando se haya hecho un click en el boton +", () => {
+        const wrapper = shallow(<NumberPicker initialCounter={0}/>);
+        wrapper.find("button").at(0).simulate("click");
+        expect(wrapper.find("h1").text()).toBe("1");
+      });
+      test("Deberia renderizar un -1 cuando se haya hecho un click en el boton -", () => {
+        const wrapper = shallow(<NumberPicker initialCounter={0}/>);
+        wrapper.find("button").at(1).simulate("click");
+        expect(wrapper.find("h1").text()).toBe("-1");
+      });
     });
   });
+
+  describe('InitialValue', () => {
+    test('debería renderizar un h1 con el valor inicial pasado por props', () => {
+      const wrapper = shallow(<NumberPicker initialCounter={20}/>);
+      expect(wrapper.find('h1').text()).toBe("20");
+    });
+  })
 });
